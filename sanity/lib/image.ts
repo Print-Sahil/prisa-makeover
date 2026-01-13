5c54cbae7b749df4b871d571ec34d1b78280ce99
Print-Sahil <kjsahil4321@gmail.com>
2026-01-13 21:33:40 +0530

    fixed image error

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,9 +1,9 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+// Error waali line ko humne niche wali line se badal diya hai
+type SanityImageSource = any;
 
 import { dataset, projectId } from '../env'
 
-// https://www.sanity.io/docs/image-url
 const builder = createImageUrlBuilder({ projectId, dataset })
 
 export const urlFor = (source: SanityImageSource) => {
